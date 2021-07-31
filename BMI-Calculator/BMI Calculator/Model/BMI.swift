0aa0f827b4703f72d7204f790dedf5ecfc514311
Aleksandr Morozov <85921199+aleksandr-mor@users.noreply.github.com>
2021-07-31 23:45:30 +0300

    Add files via upload

---
+++ BMI-Calculator/BMI Calculator/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Александр Морозов on 29.07.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
