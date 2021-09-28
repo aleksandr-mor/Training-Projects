a55c15042b1eec02dc9865ab59e142ee054f5e25
Aleksandr Morozov <dr.morozov@hotmail.com>
2021-09-28 22:44:30 +0300

    Add files via upload

---
+++ Flash-Chat (in progress)/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    
+}
