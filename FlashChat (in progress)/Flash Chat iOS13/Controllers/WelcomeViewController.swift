857aa96f7a83734b39879c906e313bddda1fa77d
Aleksandr Morozov <dr.morozov@hotmail.com>
2021-09-28 22:41:33 +0300

    Add files via upload

---
+++ FlashChat (in progress)/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
