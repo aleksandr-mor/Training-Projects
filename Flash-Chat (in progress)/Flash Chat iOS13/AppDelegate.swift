6dbddad55c2472c141174efcda78b290d0db083d
Aleksandr Morozov <dr.morozov@hotmail.com>
2021-10-02 22:45:08 +0300

    Add files via upload

--- Flash-Chat (in progress)/Flash Chat iOS13/AppDelegate.swift
+++ Flash-Chat (in progress)/Flash Chat iOS13/AppDelegate.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,7 +8,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
