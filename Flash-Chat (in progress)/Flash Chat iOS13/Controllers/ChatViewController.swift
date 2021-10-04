2c3ef0d879216f89e2f226c45b771799565a18f8
Aleksandr Morozov <dr.morozov@hotmail.com>
2021-10-04 22:58:39 +0300

    Add files via upload

--- Flash-Chat (in progress)/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat (in progress)/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -19,6 +19,7 @@ class ChatViewController: UIViewController {
         title = K.appName
         navigationItem.hidesBackButton = true
         
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -44,8 +45,8 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
         return cell
     }
 }
